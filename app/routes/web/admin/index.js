cf5f4173c3fdef66b17e35f7a39a1d8abaa7695c
Erwin <erwin0maleki@gmail.com>
2019-04-18 00:57:22 +0430

    added mutler but commented bcs we cant use it while usuing localhost

--- app/routes/web/admin/index.js
+++ app/routes/web/admin/index.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const router = express.Router();
 const Post = require('../../../models/post');
-
+const multer = require('multer');
+var globalname;
+// var storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null,'/projects/Websites/simpleBlog/assets/img')
+//     },
+//     filename: function (req, file, cb) {
+//         var extention;
+//         if (file.originalname.indexOf('.jpg') >= 0) {
+//             extention = file.originalname.indexOf('.jpg');
+//         } else if (file.originalname.indexOf('.png') >= 0) {
+//             extention = file.originalname.indexOf('.png');
+//         } else if (file.originalname.indexOf('.gif') >= 0) {
+//             extention = file.originalname.indexOf('.gif');
+//         }
+//         if (extention) {
+//             var zoom = file.originalname.substr(extention, extention + 3);
+//             var filename = file.originalname.substr(0, extention);
+//             globalname = filename + "-" + Date.now() + "-" + file.originalname.substr(extention, extention + 3);
+//         }
+//         cb(null, filename + "-" + Date.now() + "-" + file.originalname.substr(extention, extention + 3));
+//     }
+// });
+// var upload = multer({
+//     storage: storage
+// });
 router.get('/dashboard', (req, res) => {
      res.render('admin/index');
 });
@@ -16,17 +41,20 @@ router.get('/dashboard/posts', (req, res) => {
 router.get('/dashboard/posts/add', (req, res) => {
         res.render('admin/newPost');
 });
-router.post('/dashboard/posts/add', (req, res) => {
+router.post('/dashboard/posts/add', (req, res) => { //deleted upload.single('ImageFile') because in localhost we cant use local image like in the online sites
+        console.log(req.body.title);
+        console.log(req.body.imageUrl);
         let newPost = new Post({
             title:req.body.title,
             content:req.body.content,
             slug:req.body.slug,
-            img:'https://roocket.ir/public/image/2018/1/21/1516505980bootstrap4-cover-1.jpg'
+            img:req.body.imageUrl
         });
         newPost.save((err)=>{
             if(err) throw(err);
             res.redirect('/admin/dashboard/posts')
-        })
+        });
+        
 });
 router.get('/dashboard/posts/delete/:id', (req, res) => {
         let id = req.params.id;
