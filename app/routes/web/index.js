b33128eb9f9cac399cdb906a46bc0a31025b1c99
Erwin <erwin0maleki@gmail.com>
2019-12-12 05:30:37 +0330

    now you can click on categories and see relevant posts

--- app/routes/web/index.js
+++ app/routes/web/index.js
@@ -15,6 +15,19 @@ router.get('/', (req, res) => {
         })
     })
 });
+router.get('/categories/:categoryName', (req, res) => {
+    Category.find({categoryName:req.params.categoryName}).populate('posts').exec((err,post)=>{
+        if(err) throw err;
+        Category.find({},(err,categories)=>{
+            if(err) throw err;
+            return res.render('home',{
+                categories:categories,
+                posts:post[0].posts.reverse()
+            });
+        })
+        // res.json(post[0].posts);
+    })
+});
 router.get('/post/:postName', (req, res) => {
     Post.findOne({slug:req.params.postName},(err,content)=>{
 
